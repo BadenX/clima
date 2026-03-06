1664bd2b1ee1105781d641a8aa90124d5c8fd586
BadenX <188515695+BadenX@users.noreply.github.com>
2026-03-06 20:09:06 +0100

    Finishing touches

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -9,6 +9,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,17 +24,34 @@ class _CityScreenState extends State<CityScreen> {
         constraints: BoxConstraints.expand(),
         child: SafeArea(
           child: Column(
-            children: <Widget>[
+            children: [
               Align(
                 alignment: Alignment.topLeft,
                 child: TextButton(
-                  onPressed: () {},
-                  child: Icon(Icons.arrow_back_ios, size: 50.0),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 50.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              Container(padding: EdgeInsets.all(20.0), child: null),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  onChanged: (value) {
+                    cityName = value;
+                  },
+                  style: TextStyle(color: Colors.black),
+                  decoration: kTextFieldInputDecoration,
+                ),
+              ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text('Get Weather', style: kButtonTextStyle),
               ),
             ],
